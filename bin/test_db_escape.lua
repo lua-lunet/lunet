3fc3ccdb0cfee804152e449229d8a4445592b367
Simon Massey <322608+simbo1905@users.noreply.github.com>
2026-02-15 19:07:36 +0000

    chore: remove orphaned scripts (test_db_escape, test_json, generate_dev_certs)

--- bin/test_db_escape.lua
+++
@@ -1,8 +0,0 @@
-local db = require("lunet.db")
-local s = "test"
-local esc = db.escape(s)
-print("Escaping: test")
-for i=1, #esc do
-    io.write(string.format("%02X ", string.byte(esc, i)))
-end
-print("")
